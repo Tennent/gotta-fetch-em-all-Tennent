52526c264008fd150726991c7bac162a96624f9c
Tennent <zalan.marton@gmail.com>
2023-11-13 17:40:47 +0100

    handle rounting on click

--- frontend/src/components/Locations.jsx
+++ frontend/src/components/Locations.jsx
@@ -1,3 +1,7 @@
-export default function Locations({ location }) {
-    return <li>{location.name}</li>
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+export default function Locations({location}) {
+    const navigate = useNavigate();
+    return <li onClick={() => navigate(`/areas/${location.areas[0]["name"]}`)}>{location.name}</li>
 }
