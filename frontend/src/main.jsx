c634b07c54ec2be94cd75a60ee74dffb5eb8afb4
Tennent <zalan.marton@gmail.com>
2023-11-13 17:39:55 +0100

    add routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home/Home.jsx'
+import Areas from './pages/Areas/Areas.jsx'
+import Home  from './pages/Home/Home.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/areas/:name' element={<Areas />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
