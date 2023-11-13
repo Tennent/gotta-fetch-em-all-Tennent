875af143a1ce9732571edf8963800977079a4f6a
Tennent <zalan.marton@gmail.com>
2023-11-13 17:44:43 +0100

    create function for area fetching

--- frontend/src/services/FetchLocations.js
+++ frontend/src/services/FetchLocations.js
@@ -1,4 +1,4 @@
-export default async function fetchLocation(index) {
+export async function fetchLocation(index) {
     try {
         const res = await fetch(`https://pokeapi.co/api/v2/location/${index}`);
         const data = await res.json();
@@ -7,3 +7,15 @@ export default async function fetchLocation(index) {
         console.error(`Error fetching location: ${error}`);
     }
 };
+
+export async function fetchAreas(locationAreaName) {
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/location-area/${locationAreaName}`);
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(`Error fetching areas: ${error}`);
+    }
+};
+
