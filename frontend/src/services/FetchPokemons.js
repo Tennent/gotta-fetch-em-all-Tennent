794e21d4e04ed01ac8777092f2468ef7f8792985
Koterlan David <koterlandavid@gmail.com>
2023-11-14 16:03:42 +0100

    Create fetchOwnedPokemons

--- frontend/src/services/FetchPokemons.js
+++ frontend/src/services/FetchPokemons.js
@@ -2,7 +2,16 @@ export async function fetchPokemon(url) {
     try {
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(`Error fetching pokemon: ${error}`);
+    }
+};
+
+export async function fetchOwnedPokemons() {
+    try {
+        const res = await fetch("/api/pokemons");
+        const data = await res.json();
         return data;
     } catch (error) {
         console.error(`Error fetching pokemon: ${error}`);
