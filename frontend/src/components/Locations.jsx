3cf6a5864ac08635758909a76d292f17d52aefaa
Koterlan David <koterlandavid@gmail.com>
2023-11-14 11:37:28 +0100

    Render Areas when click on a Location

--- frontend/src/components/Locations.jsx
+++ frontend/src/components/Locations.jsx
@@ -1,7 +1,19 @@
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import Area from "./Area";
 
-export default function Locations({location}) {
+
+export default function Locations({ location }) {
+    const [visible, setVisible] = useState(false);
     const navigate = useNavigate();
-    return <li onClick={() => navigate(`/areas/${location.areas[0]["name"]}`)}>{location.name}</li>
+    return (
+        <div className="location-container">
+            <h2 className="location"
+                onClick={() => { visible ? setVisible(false) : setVisible(true) }}
+            >{location.name + ((location.areas.length > 0) ? "" : " (No area provided)")}</h2>
+            {
+                location.areas.map((area, i) => <Area area={area} key={i} visible={visible} />)
+            }
+        </div>
+    )
 }
