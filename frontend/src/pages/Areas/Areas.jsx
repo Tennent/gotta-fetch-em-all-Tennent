e05a8789b17fcd66059e2a86fe817efe5c2f2c7b
Koterlan David <koterlandavid@gmail.com>
2023-11-14 16:03:05 +0100

    Render user pokemons

--- frontend/src/pages/Areas/Areas.jsx
+++ frontend/src/pages/Areas/Areas.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { fetchAreas } from '../../services/FetchLocations'
-import { fetchPokemon } from '../../services/FetchPokemons'
+import { fetchPokemon, fetchOwnedPokemons } from '../../services/FetchPokemons'
 import { useEffect, useState } from 'react'
 import random from '../../utils/random'
 import './Areas.css'
@@ -10,32 +10,52 @@ import './Areas.css'
 export default function Areas() {
     const navigate = useNavigate()
     const { name } = useParams()
+
     const [encounter, setEncounter] = useState([]);
     const [pokemon, setPokemon] = useState(null);
+    const [ownedPokemons, setOwnedPokemons] = useState([]);
     useEffect(() => {
         const fetchArea = async () => {
             const area = await fetchAreas(name);
             if (area.pokemon_encounters.length > 0) setEncounter(area.pokemon_encounters);
             const pokemonUrl = area.pokemon_encounters[random(area.pokemon_encounters.length)].pokemon.url
             setPokemon(await fetchPokemon(pokemonUrl))
+
+            const pokemonUrls = await fetchOwnedPokemons()
+            const pokemonArray = pokemonUrls.pokemons.map(pokemonUrl => fetchPokemon(pokemonUrl))
+            setOwnedPokemons(await Promise.all(pokemonArray))
         }
         fetchArea()
-
     }, [])
-
-    return (
-        <div className='encounter'>
-            {(encounter) ? (
-                (pokemon) ?
+    console.log(ownedPokemons);
+    if (encounter) {
+        return (
+            <div className='encounter'>
+                {(pokemon) ?
                     <div className='pokemon'>
                         <h2>{pokemon.name}</h2>
                         <img className='pokemon-image' src={pokemon.sprites.front_default} alt="pokemon image" />
                     </div> :
-                    <p>loading...</p>) :
-                <p>This location doesn't seem to have any pokémon</p>}
-            <button type='button' onClick={() => { navigate(-1) }}>Flee</button>
-        </div>
-    )
+                    <p>loading...</p>}
+                {(ownedPokemons.length > 0) ?
+                    <div className='own-pokemons'>
+                        <h2>Own pokemons</h2>
+                        {ownedPokemons.map((pokemon, i) =>
+                            <div className='own-pokemon' key={i}>
+                                <h2>{pokemon.name}</h2>
+                                <img className='pokemon-image' src={pokemon.sprites.front_default} alt="pokemon image" />
+                            </div>
+                        )}
+                    </div> :
+                    <p>loading...</p>}
+            </div>
+        )
+    } else {
+        return (
+            <p>No encounter</p>
+        )
+    }
+
 }
 
 
