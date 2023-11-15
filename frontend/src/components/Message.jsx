2ce6d7125344bc865a5ae453688a8f2c42c13b2c
Tennent <zalan.marton@gmail.com>
2023-11-15 16:11:05 +0100

    update message component structure

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Message({message}) {
+export default function Message({ message }) {
   return (
     <div id='message-box'>
-    <p>{message}</p>
+      <p key={message} id='message' data-text={message}></p>
     </div>
   )
 }
