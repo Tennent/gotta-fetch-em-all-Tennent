91300880892a3d848d0148b037ff607148fdde7a
Tennent <zalan.marton@gmail.com>
2023-11-15 12:33:54 +0100

    store pokemons in states and use external components for display

--- frontend/src/pages/Areas/Areas.jsx
+++ frontend/src/pages/Areas/Areas.jsx
@@ -4,6 +4,8 @@ import { fetchAreas } from '../../services/FetchLocations'
 import { fetchPokemon, fetchOwnedPokemons } from '../../services/FetchPokemons'
 import { useEffect, useState } from 'react'
 import random from '../../utils/random'
+import Pokemon from '../../components/Pokemon'
+import BattleButton from '../../components/BattleButton'
 import './Areas.css'
 
 
@@ -12,14 +14,15 @@ export default function Areas() {
     const { name } = useParams()
 
     const [encounter, setEncounter] = useState([]);
-    const [pokemon, setPokemon] = useState(null);
+    const [enemyPokemon, setEnemyPokemon] = useState(null);
     const [ownedPokemons, setOwnedPokemons] = useState([]);
+    const [selectedPokemon, setSelectedPokemon] = useState(null);
     useEffect(() => {
         const fetchArea = async () => {
             const area = await fetchAreas(name);
             if (area.pokemon_encounters.length > 0) setEncounter(area.pokemon_encounters);
             const pokemonUrl = area.pokemon_encounters[random(area.pokemon_encounters.length)].pokemon.url
-            setPokemon(await fetchPokemon(pokemonUrl))
+            setEnemyPokemon(await fetchPokemon(pokemonUrl))
 
             const pokemonUrls = await fetchOwnedPokemons()
             const pokemonArray = pokemonUrls.pokemons.map(pokemonUrl => fetchPokemon(pokemonUrl))
@@ -28,26 +31,27 @@ export default function Areas() {
         fetchArea()
     }, [])
     console.log(ownedPokemons);
+    console.log(selectedPokemon);
     if (encounter) {
         return (
             <div className='encounter'>
-                {(pokemon) ?
-                    <div className='pokemon'>
-                        <h2>{pokemon.name}</h2>
-                        <img className='pokemon-image' src={pokemon.sprites.front_default} alt="pokemon image" />
-                    </div> :
+                {(enemyPokemon) ?
+                    <Pokemon pokemon={enemyPokemon} /> :
                     <p>loading...</p>}
-                {(ownedPokemons.length > 0) ?
-                    <div className='own-pokemons'>
-                        <h2>Own pokemons</h2>
-                        {ownedPokemons.map((pokemon, i) =>
-                            <div className='own-pokemon' key={i}>
-                                <h2>{pokemon.name}</h2>
-                                <img className='pokemon-image' src={pokemon.sprites.front_default} alt="pokemon image" />
-                            </div>
-                        )}
-                    </div> :
-                    <p>loading...</p>}
+                {selectedPokemon ?
+                    <>
+                        <Pokemon pokemon={selectedPokemon} />
+                        <BattleButton selectedPokemon={selectedPokemon} enemyPokemon={enemyPokemon} />
+                    </>
+                    : (ownedPokemons.length > 0) ?
+                        <div className='own-pokemons'>
+                            <h2>Own pokemons</h2>
+                            <h3>Please choose a Pokemon</h3>
+                            {ownedPokemons.map((pokemon, i) =>
+                                <Pokemon key={i} pokemon={pokemon} setSelectedPokemon={() => setSelectedPokemon(pokemon)} />
+                            )}
+                        </div> :
+                        <p>loading...</p>}
             </div>
         )
     } else {
