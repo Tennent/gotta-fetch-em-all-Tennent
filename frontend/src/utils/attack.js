438b7bf3f84fac37e25f0656bf8d49121d18353d
Tennent <zalan.marton@gmail.com>
2023-11-15 12:36:21 +0100

    handle pokemon attack logic

---
+++ frontend/src/utils/attack.js
@@ -0,0 +1,19 @@
+import fetchMove from "../services/FetchMove"
+import random from "./random"
+
+export default async function attack(attackerPokemon, defenderPokemon) {
+
+    let randomMove = await fetchMove(attackerPokemon.moves[random(attackerPokemon.moves.length)].move.url)
+    let attackerTypes = attackerPokemon.types.map(type => type.type.name)
+    const typeModifiers = [40, 20, 10, 5, 2.5, 0]
+
+    const A = 1 // attacker's Level
+    const B = attackerPokemon.stats[1].base_stat // attacker's Attack or Special
+    const C = randomMove.power || 10 // attack Power
+    const D = defenderPokemon.stats[2].base_stat // defender's Defense or Special
+    const X = attackerTypes.includes(randomMove.type.name) ? 1.5 : 1 // same-Type attack bonus (1 or 1.5)
+    const Y = typeModifiers[random(typeModifiers.length)] // Type modifiers (40, 20, 10, 5, 2.5, or 0)
+    const Z = 217 + random(255-217) // a random number between 217 and 255
+
+    return Math.ceil(((((((((2 * A / 5 + 2) * B * C) / D) / 50) + 2) * X) * Y / 10) * Z) / 255);
+}
