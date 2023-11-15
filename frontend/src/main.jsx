236c6dc924ff9039d68446b749df51d85a8e6908
Tennent <zalan.marton@gmail.com>
2023-11-15 12:30:36 +0100

    add route for battles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Areas from './pages/Areas/Areas.jsx'
 import Home  from './pages/Home/Home.jsx'
+import Battle from './pages/Battle/Battle.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './main.css'
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/areas/:name' element={<Areas />} />
+        <Route path='/battle' element={<Battle />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
