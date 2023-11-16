bdd45e3dbd1f6d5305e6c8770c5d04e522838a8d
Tennent <zalan.marton@gmail.com>
2023-11-16 10:37:14 +0100

    add hp bar for pokemons

--- frontend/src/components/Pokemon.jsx
+++ frontend/src/components/Pokemon.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export default function Pokemon({ pokemon, setSelectedPokemon, health, className }) {
+export default function Pokemon({ pokemon, setSelectedPokemon, health, maxHp, className }) {
     return (
         <div className={className} onClick={setSelectedPokemon}>
             <h2>{pokemon.name}</h2>
             <img className='pokemon-image' src={pokemon.sprites.front_default} alt="pokemon image" />
-            {health ? (<p>{health}</p>) : ""}
+            {health ?
+                <div className="progress" style={{ width: maxHp }}>
+                    <div className="progress-bar" role="progressbar" aria-valuenow={health} style={{ width: health }} aria-valuemin={health} aria-valuemax={health}>{health}</div>
+                </div> :
+                ""}
         </div>
     )
 }
